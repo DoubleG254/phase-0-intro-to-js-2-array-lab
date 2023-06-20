7694b2a0dc256dc59fbe8e6f1aed0cc90f7ee7f6
DoubleG254 <glorygweth6@gmail.com>
2023-06-20 10:49:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]// Write your solution here!
+beforeEach(function ArrayFunctions(){
+    cats.length = o
+    
+    cats.push ('Milo', 'Otis', 'Garfield');
+})
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function removeLastCat(){
+   let newcats= cats.slice(0, -1)
+    return newcats
+}
+function appendCat(name){
+    let newcats=[...cats,"Broom"]
+    return newcats
+}
+function prependCat(name){
+    let newcats=["Arnold",...cats]
+    return newcats
+}
+function removeFirstCat(){
+    let newcats= cats.slice(1)
+    return newcats
+}
